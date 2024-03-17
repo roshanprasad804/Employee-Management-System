f614aa633440de755a6cbaf9d84d9b4c2380e9d9
roshanprasad804 <82306250+roshanprasad804@users.noreply.github.com>
2024-03-17 18:30:33 +0530

    Get API Integration

--- EMS-UI/src/app/employee/employee.component.ts
+++ EMS-UI/src/app/employee/employee.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { EmployeeService } from '../services/employee.service';
 
 @Component({
   selector: 'app-employee',
@@ -9,11 +10,26 @@ import { Component } from '@angular/core';
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
+  constructor( private employeeservice:EmployeeService) {
+
+  }
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
   employees: any[] = [
     { id: 1, name: 'John Doe', department: 'IT' },
     { id: 2, name: 'Jane Smith', department: 'HR' },
     { id: 3, name: 'Alice Johnson', department: 'Finance' }
   ];
+
+  getEmployees() {
+    this.employeeservice.getEmployees().subscribe((data:any) => {
+      console.log('data',data);
+      // this.employees.map = data;
+    });
+  }
 
   addEmployee() {
     
---
+++ EMS-UI/src/app/services/employee.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(private http:HttpClient) { }
+
+  getEmployees() {
+    return this.http.get('http://localhost:8080/api/employees');
+  }
+}
